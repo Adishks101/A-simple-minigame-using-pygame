afd289b3c898e326da5406a90b63acee10435227
StarLord <37327630+Adishks101@users.noreply.github.com>
2021-03-17 11:15:22 +0530

    Update game.py

--- game.py
+++ game.py
@@ -36,12 +36,12 @@ youwin = pygame.image.load("resources/images/youwin.png")
 hit = pygame.mixer.Sound("resources/audio/explode.wav")
 enemy = pygame.mixer.Sound("resources/audio/enemy.wav")
 shoot = pygame.mixer.Sound("resources/audio/shoot.wav")
-hit.set_volume(0.05)
-enemy.set_volume(0.05)
-shoot.set_volume(0.05)
-pygame.mixer.music.load('resources/audio/moonlight.wav')
+hit.set_volume(0.15)
+enemy.set_volume(0.15)
+shoot.set_volume(0.15)
+#pygame.mixer.music.load('the background music file')
 pygame.mixer.music.play(-1, 0.0)
-pygame.mixer.music.set_volume(0.25)
+pygame.mixer.music.set_volume(0.50)
 
 
 
